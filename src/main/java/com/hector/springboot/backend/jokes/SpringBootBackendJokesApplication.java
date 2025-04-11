d35a1f42d7d613966b8b2cd0b7b169bd719d1988
Héctor Rodríguez Planelles <hectorrodriguezplanelles@gmail.com>
2025-04-11 16:18:17 +0200

    Configuracion Proyecto

---
+++ src/main/java/com/hector/springboot/backend/jokes/SpringBootBackendJokesApplication.java
@@ -0,0 +1,13 @@
+package com.hector.springboot.backend.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hector/springboot/backend/jokes/SpringBootBackendJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hector.springboot.backend.jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
